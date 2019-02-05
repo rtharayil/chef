9c6c97d6282e1e1695f0a21e5cb37b508c4d5f6b
rtharayl <you@example.com>
2019-02-05 17:25:12 +0000

    hello world

--- installForUbuntu16_04.sh
+++ installForUbuntu16_04.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 wget https://packages.chef.io/files/stable/chefdk/3.7.23/ubuntu/16.04/chefdk_3.7.23-1_amd64.deb
 dpkg -i chefdk_3.7.23-1_amd64.deb
+eval "$(chef shell-init bash)"
