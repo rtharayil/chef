8f9ab6fcb2c6affc987eaba79cfcf4a10df8431b
rtharayl <you@example.com>
2019-02-16 12:32:53 +0000

    add user exmaple

--- installForUbuntu16_04.sh
+++ installForUbuntu16_04.sh
@@ -2,3 +2,4 @@
 wget https://packages.chef.io/files/stable/chefdk/3.7.23/ubuntu/16.04/chefdk_3.7.23-1_amd64.deb
 dpkg -i chefdk_3.7.23-1_amd64.deb
 eval "$(chef shell-init bash)"
+eval "$(chef shell-init bash)"' >> ~/.bashrc
---
+++ userExercise/tdi.rb
@@ -0,0 +1,7 @@
+user 'tdi' do
+  action :create
+  comment "Test Driven Infrastructure"
+  home "/home/tdi"
+  manage_home true
+end
+
